51ae88a67ddbe6f42dc5bcce67aea733fabe33f5
synergy2411 <synergy2411@outlook.com>
2018-09-25 17:42:21 +0530

    Initial commit

---
+++ module-basics.js
@@ -0,0 +1,31 @@
+
+// 	PATH Module
+
+    // var path = require('path');
+    // var url = "http://www.example.com/public/index.html";
+    // console.log(path.dirname(url));
+    // console.log(path.extname(url));
+    // console.log(path.basename(url));
+
+// 	OS Module
+    // var os = require("os");
+    // console.log("Arch : " + os.arch());
+    // console.log("Number of CPU's : " + os.cpus().length);
+    // console.log("Total Memory : " + os.totalmem());
+    // console.log("Free Memory : " + os.freemem());
+
+// 	HTTP Module
+    // var http = require("http");
+    // var server = http.createServer(function (request, response){
+    //     console.log("Method : " + request.method);
+    //     response.writeHead(200);
+    //     response.write("Hello from Node!");
+    //     response.end();
+    // })
+    // server.listen(9090);
+    // console.log("Server started at port 9090...");
+
+
+// 	Events Module
+//  FS (FileSystem) 
+// 	UTIL Module
